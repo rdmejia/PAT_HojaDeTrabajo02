f9e2020ce6ac6e698c49a584d724ead4a90c9822
harriett 3 <144355411+harriett-gl@users.noreply.github.com>
2023-10-22 16:35:32 -0600

    Update Ejercicio01.h

--- PAT_HojaDeTrabajo02/Ejercicio01.h
+++ PAT_HojaDeTrabajo02/Ejercicio01.h
@@ -5,6 +5,33 @@
 class Ejercicio01
 {
 public:
-	Node<char>* rotateRight(Node<char>* head, int k);
+	Node<char>* rotateRight(Node<char>* head, int k) {
+        if (head == nullptr || head->next == nullptr || k == 0) {
+            return head;
+        }
+
+        int len = 1;
+        Node<char>* tail = head;
+        while (tail->next != nullptr) {
+            tail = tail->next;
+            len++;
+        }
+
+        k %= len;
+        if (k == 0) {
+            return head;
+        }
+
+        Node<char>* new_tail = head;
+        for (int i = 0; i < len - k - 1; i++) {
+            new_tail = new_tail->next;
+        }
+
+        Node<char>* new_head = new_tail->next;
+        new_tail->next = nullptr;
+        tail->next = head;
+
+        return new_head;
+    }
 };
 
