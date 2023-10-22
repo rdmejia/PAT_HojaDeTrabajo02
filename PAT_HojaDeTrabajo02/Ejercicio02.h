a62f142ed9ed6d9f1b675cef19337f3a063ce127
harriett 3 <144355411+harriett-gl@users.noreply.github.com>
2023-10-22 16:36:42 -0600

    Update Ejercicio02.h

--- PAT_HojaDeTrabajo02/Ejercicio02.h
+++ PAT_HojaDeTrabajo02/Ejercicio02.h
@@ -5,6 +5,30 @@
 class Ejercicio02
 {
 public:
-	Node<int>* addTwoNumbers(Node<int>* l1, Node<int>* l2);
+	Node<int>* addTwoNumbers(Node<int>* l1, Node<int>* l2){
+		    Node<int> dummy(0);
+    Node<int>* current = &dummy;
+    int carry = 0;
+
+    while (l1 || l2 || carry) {
+        int sum = carry;
+
+        if (l1) {
+            sum += l1->value;
+            l1 = l1->next;
+        }
+
+        if (l2) {
+            sum += l2->value;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+        current->next = new Node<int>(sum % 10);
+        current = current->next;
+    }
+
+    return dummy.next;
+}
 };
 
