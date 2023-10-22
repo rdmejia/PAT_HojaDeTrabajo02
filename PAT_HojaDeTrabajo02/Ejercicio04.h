c64ac2d7db278898502bced08014db347f24356a
harriett 3 <144355411+harriett-gl@users.noreply.github.com>
2023-10-22 16:41:05 -0600

    Update Ejercicio04.h

--- PAT_HojaDeTrabajo02/Ejercicio04.h
+++ PAT_HojaDeTrabajo02/Ejercicio04.h
@@ -4,16 +4,49 @@ class FrontMiddleBackQueue
 public:
 	FrontMiddleBackQueue();
 
-	void pushFront(int value);
+	void pushFront(int value) {
+        data_.push_front(value);
+    }
 
-	void pushMiddle(int value);
+	void pushMiddle(int value) {
+        auto it = data_.begin();
+        std::advance(it, data_.size() / 2);
+        data_.insert(it, value);
+    }
 
-	void pushBack(int value);
+	void pushBack(int value) {
+        data_.push_back(value);
+    }
 
-	int popFront();
+	int popFront() {
+        if (data_.empty()) {
+            return -1;
+        }
+        int front = data_.front();
+        data_.pop_front();
+        return front;
+    }
 
-	int popMiddle();
+	int popMiddle() {
+        if (data_.empty()) {
+            return -1;
+        }
+        auto it = data_.begin();
+        std::advance(it, (data_.size() - 1) / 2);
+        int middle = *it;
+        data_.erase(it);
+        return middle;
+    }
 
-	int popBack();
+	int popBack() {
+        if (data_.empty()) {
+            return -1;
+        }
+        int back = data_.back();
+        data_.pop_back();
+        return back;
+    }
+private:
+    std::list<int> data_;
 };
 
